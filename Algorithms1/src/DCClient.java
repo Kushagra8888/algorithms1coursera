c3c314e6411da0b3bdc6c8b4b28e114e0f7fd64c
Kushagra Sharma <kushagra8888@gmail.com>
2014-06-26 23:59:02 +0530

    Implemented QuickFindUF class

---
+++ Algorithms1/src/DCClient.java
@@ -0,0 +1,20 @@
+
+
+public class DCClient {
+
+	public static void main(String[] args) {
+		int N = StdIn.readInt();
+		QuickFindUF uf = new QuickFindUF(N);
+		while(!StdIn.isEmpty())
+		{
+			int p = StdIn.readInt();
+			int q = StdIn.readInt();
+			if (!uf.connected(p, q))
+			{
+				uf.union(p, q);
+				StdOut.println(p + " ");
+			}
+		}
+	}
+
+}
---
+++ Algorithms1/src/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains classes to implement the dynamic connectivity problem
+ * following the specifications laid out iin Algorithms 1 course on Coursera
+ */
+/**
+ * @author Kushagra
+ *
+ */
