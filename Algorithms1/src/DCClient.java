fb7e258f3ef6356cd3a5f87b2e70425675e822b6
Kushagra Sharma <kushagra8888@gmail.com>
2014-06-29 21:24:51 +0530

    Running version of PercolationStats

--- Algorithms1/src/DCClient.java
+++ Algorithms1/src/DCClient.java
@@ -1,8 +1,16 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 
 public class DCClient {
 
 	public static void main(String[] args) {
+		try {
+			System.setIn(new FileInputStream("D:/Workbook/Git/Algorithms1/Algorithms1/test_files/tinyUf.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
 		int N = StdIn.readInt();
 		QuickFindUF uf = new QuickFindUF(N);
 		while(!StdIn.isEmpty())
@@ -12,7 +20,8 @@ public class DCClient {
 			if (!uf.connected(p, q))
 			{
 				uf.union(p, q);
-				StdOut.println(p + " ");
+				StdOut.println(p + " " + q);
+				//System.out.println(p + " ");
 			}
 		}
 	}
--- Algorithms1/src/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contains classes to implement the dynamic connectivity problem
- * following the specifications laid out iin Algorithms 1 course on Coursera
- */
-/**
- * @author Kushagra
- *
- */
